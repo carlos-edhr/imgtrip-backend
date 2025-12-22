c77241ec6cd219064bdd1a8e612d8f1d58ae66bf
Eduardo Rojas <carlos.edhr@protonmail.com>
2025-12-21 16:44:55 -0800

    feat: added activity stream functionality.

--- account/views.py
+++ account/views.py
@@ -3,6 +3,8 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse, JsonResponse
 from django.views.decorators.http import require_POST
 from django.shortcuts import render, get_object_or_404
+from actions.utils import create_action
+from actions.models import Action
 from django.contrib import messages
 from .forms import (
     LoginForm,
@@ -56,7 +58,18 @@ def user_login(request):
 
 @login_required
 def dashboard(request):
-    return render(request, "account/dashboard.html", {"section": "dashboard"})
+    # display all actions by defualt
+    actions = Action.objects.exclude(user=request.user)
+    following_ids = request.user.following.values_list("id", flat=True)
+    if following_ids:
+        # If user is following others, retrieve only their actions
+        actions = actions.filter(user_id__in=following_ids)
+    acitons = actions.select_related("user", "user__profile").prefetch_related(
+        "target"
+    )[:10]
+    return render(
+        request, "account/dashboard.html", {"section": "dashboard", "actions": actions}
+    )
 
 
 def register(request):
@@ -71,6 +84,7 @@ def register(request):
             new_user.save()
             # Create the user profile
             Profile.objects.create(user=new_user)
+            create_action(new_user, "has created an account")
             return render(
                 request,
                 "account/register_done.html",
@@ -117,6 +131,7 @@ def user_follow(request):
             user = User.objects.get(id=user_id)
             if action == "follow":
                 Contact.objects.get_or_create(user_from=request.user, user_to=user)
+                create_action(request.user, "is following", user)
             else:
                 Contact.objects.filter(user_from=request.user, user_to=user).delete()
         except User.DoesNotExist:
